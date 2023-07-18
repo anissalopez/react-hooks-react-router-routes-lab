e5f14b0c70794c09bc28a683a8fa3cca03ac58c9
anissalopez <anissa965@gmail.com>
2023-07-18 11:50:17 -0400

    react routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+
+  return <div className="navbar">
+    <nav>
+    <NavLink exact to="/" >Home</NavLink>
+    <NavLink to="/Actors" >Actors</NavLink>
+    <NavLink to="/Directors" >Directors</NavLink>
+    <NavLink to="/Movies">Movies</NavLink>
+    </nav>
+    
+    
+    </div>;
 }
 
 export default NavBar;
